eb4624cbc0b1aecb2a83eddc587af17f44b679c6
zhangcheng <xyzc1988@163.com>
2016-12-28 18:26:26 +0800

    day-8,构建前端

--- www/handlers.py
+++ www/handlers.py
@@ -9,11 +9,23 @@ import re, time, json, logging, hashlib, base64, asyncio
 from coroweb import get,post
 from Models import User,Blog,Comment,next_id
 
+# @get('/')
+# async def index(request):
+#     users = await User.findAll()
+#     return {
+#         '__template__':'test.html',
+#         'users' : users
+#     } 
+#     
 @get('/')
-async def index(request):
-    users = await User.findAll()
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
-        '__template__':'test.html',
-        'users' : users
-    } 
-    
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
